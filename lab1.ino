d26cdb63121c91fdb086d9f748a4f0b3d1a78b8e
Ilya1521 <49120381+Ilya1521@users.noreply.github.com>
2024-11-08 17:37:40 +0300

    Add files via upload

---
+++ lab1.ino
@@ -0,0 +1,23 @@
+//#include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+//#include <DHT.h>
+DHT dht(5, DHT11);
+
+void setup() {
+  // put your setup code here, to run once:
+  dht.begin();
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print("\nВлажность: ");
+  Serial.print(h);
+  Serial.print("\nТемпература: ");
+  Serial.print(t);
+}
